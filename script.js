f806e5103a223159ee6be145af38c92d9459be00
Jeff <jfrodriguez82@gmail.com>
2021-10-13 21:31:22 +0200

    JS creation

---
+++ script.js
@@ -0,0 +1,10 @@
+let newGame = document.getElementById('newGame')
+let rollDice = document.getElementById('rollDice')
+let hold = document.getElementById('hold')
+let score1Global = document.getElementById('score1Global')
+let score2Global = document.getElementById('score2Global')
+let score1Current = document.getElementById('score1')
+let score2Current = document.getElementById('score2')
+
+
+
